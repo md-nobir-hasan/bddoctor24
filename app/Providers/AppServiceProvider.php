5ffefecee7289eb12ac56a6d9fd4a3e5dc60cf16
Md. Nobir Hasan <nobir.wd@gmail.com>
2024-06-03 17:37:07 +0600

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\NSidebar;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,10 +26,11 @@ class AppServiceProvider extends ServiceProvider
             database_path('migrations'),
             database_path('migrations/backend'),
         ]);
-        Cache::forget('nsidebar');
-        Cache::rememberForever('nsidebar', function () {
-            return NSidebar::with('child_bar')->where('is_parent', 1)->where('status', 'Active')->get();
-        });
+
+        // Cache::forget('nsidebar');
+        // Cache::rememberForever('nsidebar', function () {
+        //     return NSidebar::with('child_bar')->where('is_parent', 1)->where('status', 'Active')->get();
+        // });
         $sidebar_lists = Cache::get('nsidebar') ?? [];
 
         view()->share('sidebar_lists',$sidebar_lists);
