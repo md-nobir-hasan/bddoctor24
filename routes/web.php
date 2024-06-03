a8777176a51b12a82b1c6cee5f37f430d30b96d0
Md. Nobir Hasan <nobir.wd@gmail.com>
2024-06-03 20:19:50 +0600

    change dashboard, remove backend route

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,8 @@
 <?php
  require __DIR__ . '/backend.php';
 
-use App\Http\Controllers\MicelController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 
 
 Route::middleware('auth')->group(function () {
