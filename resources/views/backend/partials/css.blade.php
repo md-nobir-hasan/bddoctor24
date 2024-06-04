40ea23a00445d6772083e1805a60a7d506a8284b
Md. Nobir Hasan <nobir.wd@gmail.com>
2024-06-04 07:16:25 +0600

    Update css.blade.php

--- resources/views/backend/partials/css.blade.php
+++ resources/views/backend/partials/css.blade.php
@@ -1,2 +1,3 @@
 @vite(['resources/css/app.css', 'resources/js/app.js'])
-{{-- <link rel="stylesheet" href="/src/css/customizer.css"> --}}
+<link rel="stylesheet" href="/src/css/customizer.css">
+{{-- <link rel="stylesheet" href="/src/css/style.css"> --}}
