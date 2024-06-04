23822743e01c0be87f05ff413f50c182e2439f2a
Md. Nobir Hasan <nobir.wd@gmail.com>
2024-06-04 13:33:21 +0600

    Update backend.php

--- routes/backend.php
+++ routes/backend.php
@@ -3,25 +3,18 @@
 use App\Http\Controllers\MicelController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('backend')->group(function () {
+// Route::prefix('backend')->group(function () {
     Route::middleware('auth')->prefix('setup')->name('setup.')->group(function () {
         Route::resource('/consultant-type', 'App\Http\Controllers\Backend\ConsultantTypeController');
         Route::resource('/category', 'App\Http\Controllers\Backend\CategoryController');
         Route::resource('/district', 'App\Http\Controllers\Backend\DistrictController');
+        Route::resource('/degree', 'App\Http\Controllers\Backend\DegreeController');
+        Route::resource('/designation', 'App\Http\Controllers\Backend\DesignationController');
+        Route::resource('/chamber', 'App\Http\Controllers\Backend\ChamberController');
     });
-    Route::get('/dashboard',[MicelController::class,'dashboard'])->middleware(['auth'])->name('dashboard');
-});
-
-Route::middleware('auth')->prefix('setup')->name('setup.')->group(function () {
-	Route::resource('/degree','App\Http\Controllers\DegreeController');
-Route::resource('/degree','App\Http\Controllers\Backend\DegreeController');
-Route::resource('/designation','App\Http\Controllers\DesignationController');
-Route::resource('/designation','App\Http\Controllers\Backend\DesignationController');
-Route::resource('/chamber','App\Http\Controllers\ChamberController');
-Route::resource('/chamber','App\Http\Controllers\Backend\ChamberController');
-});
+    // });
 
 Route::middleware('auth')->group(function () {
-	Route::resource('/doctor','App\Http\Controllers\DoctorController');
-Route::resource('/doctor','App\Http\Controllers\Backend\DoctorController');
+    Route::get('/dashboard', [MicelController::class, 'dashboard'])->middleware(['auth'])->name('dashboard');
+    Route::resource('/doctor', 'App\Http\Controllers\Backend\DoctorController');
 });
