06a87c802de1c445be1fa212959534f2ec8d7a59
Md. Nobir Hasan <nobir.wd@gmail.com>
2024-06-04 10:54:47 +0600

    Update backend.php

--- routes/backend.php
+++ routes/backend.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\MicelController;
 use Illuminate\Support\Facades\Route;
 
 // Route::prefix('backend')->group(function () {
-Route::middleware('auth')->prefix('setup')->name('setup.')->group(function () {
+Route::prefix('setup')->name('setup.')->group(function () {
     Route::resource('/consultant-type', 'App\Http\Controllers\Backend\ConsultantTypeController');
     Route::resource('/category', 'App\Http\Controllers\Backend\CategoryController');
     Route::resource('/district', 'App\Http\Controllers\Backend\DistrictController');
