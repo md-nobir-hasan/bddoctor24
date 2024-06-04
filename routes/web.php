1f44b187b8765158c4174fb6df8cd4a9de045c9b
Md. Nobir Hasan <nobir.wd@gmail.com>
2024-06-04 23:34:55 +0600

    dashboard change

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ use App\Http\Controllers\MicelController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
 
