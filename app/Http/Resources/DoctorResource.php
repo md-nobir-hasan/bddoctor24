9adf134234fd0f33139a00947409c6f663551082
Md. Nobir Hasan <nobir.wd@gmail.com>
2024-06-05 12:47:50 +0600

    Update DoctorResource.php

--- app/Http/Resources/DoctorResource.php
+++ app/Http/Resources/DoctorResource.php
@@ -16,6 +16,15 @@ class DoctorResource extends JsonResource
     {
         return [
             'title' => $this->title,
+            'designation_id'=>$this->gendar,
+            'category_id'=>$this->gendar,
+            'gendar'=>$this->gendar,
+            'experience'=>$this->experience,
+            'degree_id'=>$this->gendar,
+            'consultant_type_id'=>$this->gendar,
+            'chamber_id'=>$this->gendar,
+            'district_id'=>$this->gendar,
+            'other_info'=>$this->other_info,
             'serial' => $this->serial,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
