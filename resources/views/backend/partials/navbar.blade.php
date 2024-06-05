a29c1de31a20a3dac7018f1820c4578f010ced0d
Md. Nobir Hasan <nobir.wd@gmail.com>
2024-06-05 11:45:12 +0600

    Update navbar.blade.php

--- resources/views/backend/partials/navbar.blade.php
+++ resources/views/backend/partials/navbar.blade.php
@@ -425,13 +425,16 @@
             <hr class="my-0 border-t border-gray-200 dark:border-gray-700">
           </li>
           <li class="relative">
-            <a class="block clear-both w-full px-6 py-2 whitespace-nowrap hover:text-indigo-500" href="/#">
+            <form action="{{route('logout')}}" method="POST">
+                @csrf
+            <button class="block clear-both w-full px-6 py-2 whitespace-nowrap hover:text-indigo-500" href="/#">
               <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline w-4 h-4 me-2 bi bi-box-arrow-in-right" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"/>
                 <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
               </svg>
-              <!-- <i class="me-2 fas fa-sign-out-alt"></i> --> Sign out
-            </a>
+               Sign out
+            </button>
+            </form>
           </li>
         </ul>
       </li>
