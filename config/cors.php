4898c6b06a43bcc63cdbca7deb72589868c6632f
Md. Nobir Hasan <nobir.wd@gmail.com>
2024-06-11 11:56:07 +0600

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000','*'],
+    'allowed_origins' => ['https://treatmentopportunity.com/','http://localhost:3000','*'],
 
     'allowed_origins_patterns' => [],
 
